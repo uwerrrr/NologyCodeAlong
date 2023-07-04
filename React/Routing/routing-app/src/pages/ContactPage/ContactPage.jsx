285c5f5e6625ff0fd21fb6addae418761cc78c6e
Oscar (Van NGUYEN) <32070259+uwerrrr@users.noreply.github.com>
2023-07-04 15:58:16 +1000

    add contact page

--- React/Routing/routing-app/src/pages/ContactPage/ContactPage.jsx
+++ React/Routing/routing-app/src/pages/ContactPage/ContactPage.jsx
@@ -1,10 +1,21 @@
-import React from 'react';
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
 
 const ContactPage = () => {
+  const schema = yup.object({
+    name: yup.string().required("Please enter your name"),
+  });
+
+
+
   return (
-    <main>
-      <h2>Contact Page</h2>
-    </main>
+    <form>
+      <div>
+        <label htmlFor="">Name</label>
+        <input type="text" />
+      </div>
+    </form>
   );
 };
 
--- React/Routing/routing-app/src/pages/LandingPage/LandingPage.jsx
+++ React/Routing/routing-app/src/pages/LandingPage/LandingPage.jsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React from "react";
+import { LoadingSpinner } from "../../components/LoadingSpinner/LoadingSpinner";
 
 const LandingPage = () => {
   return (
